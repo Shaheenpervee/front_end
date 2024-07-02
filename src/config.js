dcca3efbfd5c8be007799760236e55aebc25dfae
Shaheenpervee <167050233+Shaheenpervee@users.noreply.github.com>
2024-07-01 18:25:40 -0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 //export const url="https://fastapi-lvps.onrender.com/";
-export const url="https://science-clone.onrender.com"
+export const url="https://science-clone.onrender.com/"
 //export  const url="http://localhost:9000/";
